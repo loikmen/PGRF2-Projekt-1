423a1842fadec22e8fe66b228e089af0aea5f5b9
Milan Košťák <milankostak@seznam.cz>
2022-02-09 18:21:22 +0100

    views + AppStart

---
+++ src/view/Panel.java
@@ -0,0 +1,31 @@
+package view;
+
+import rasterize.ImageBuffer;
+import transforms.Col;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel extends JPanel {
+
+    private final ImageBuffer raster;
+
+    private static final int WIDTH = 800, HEIGHT = 600;
+
+    Panel() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        raster = new ImageBuffer(WIDTH, HEIGHT);
+        raster.setClearValue(new Col(Color.BLACK.getRGB()));
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+//        raster.repaint(g); TODO
+    }
+
+    public ImageBuffer getImageBuffer() {
+        return raster;
+    }
+
+}
---
+++ src/view/Window.java
@@ -0,0 +1,30 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+
+    private final Panel panel;
+
+    public Window() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("PGRF2");
+
+        panel = new Panel();
+
+        add(panel, BorderLayout.CENTER);
+        pack();
+
+        setLocationRelativeTo(null);
+
+        // better at the end so that the focus is not taken by another component in a possibly more complex UI
+        panel.setFocusable(true);
+        panel.grabFocus(); // important for keyboard control
+    }
+
+    public Panel getPanel() {
+        return panel;
+    }
+
+}
